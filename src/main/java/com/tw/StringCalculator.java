6b3511c0b7ff484186793d5424fc7a60083bbddf
TACULMA2 <marigold.taculog@oocl.com>
2023-08-09 16:28:00 +0800

    Implementation for String Calculator edited
    Ready to commit

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,30 @@
 package com.tw;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        if (string.isEmpty()) {
+            return 0;
+        }
+
+        String uniqueCharacter = ",";
+        String numbersSegment = string;
+
+        if (string.startsWith("//")) {
+            String[] index = string.split("\n", 2);
+            uniqueCharacter = index[0].substring(2);
+            numbersSegment = index[1];
+        }
+
+        String[] numbers = numbersSegment.split("[,\n" + Pattern.quote(uniqueCharacter) + "]");
+        int sum = 0;
+
+        for (String number : numbers) {
+            if (!number.isEmpty()) {
+                sum += Integer.parseInt(number);
+            }
+        }
+        return sum;
     }
 }
